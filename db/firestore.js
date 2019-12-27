bb1d5e2a6e5c2efb4e6aaa2f90252b76d2d72b2c
Timothy Teoh <timothy.teoh@frogasia.com>
2019-12-27 15:23:39 +0800

    chore: Change svc account name to be generic

--- db/firestore.js
+++ db/firestore.js
@@ -1,5 +1,5 @@
 //setting up database
 const { Firestore } = require('@google-cloud/firestore')
-const firestore = new Firestore({ keyFilename: './credentials/todo-svcaccount.json'});
+const firestore = new Firestore({ keyFilename: './credentials/svc-account.json'});
 
 module.exports = firestore
